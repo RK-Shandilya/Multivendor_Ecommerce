a3d089cc9372402bfdb50a7eb24015adca3d47d2
Rupesh <rkshandilya001@gmail.com>
2025-01-06 23:39:36 +0530

    working on sidebar

--- app/(Backend)/dashboard/layout.tsx
+++ app/(Backend)/dashboard/layout.tsx
@@ -8,7 +8,7 @@ const layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className='flex'>
       <Sidebar/>
-      <div className="w-full ml-60">
+      <div className="lg:ml-64 ml-0 flex-grow bg-slate-100 min-h-screen">
         <Navbar/>
         <main className='p-8 bg-slate-100 dark:bg-slate-900 dark:text-slate-50 min-h-screen mt-16'>
           {children}
--- components/Backend/Sidebar.tsx
+++ components/Backend/Sidebar.tsx
@@ -1,21 +1,139 @@
+"use client"
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import logo from '@/public/logo-light.png'
+
+  import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+  } from "@/components/ui/collapsible"
+  
+  
+import { ChevronRight, Compass, ExternalLink, LayoutGrid, LogOut, Settings, Slack, User, Users2, UserSquare2, Warehouse } from 'lucide-react'
+import { usePathname } from 'next/navigation'
+import { title } from 'process'
+
 
 const Sidebar = () => {
+    const pathname = usePathname();
+    const sidebarLinks = [
+        {
+            title: "Customers",
+            icon:Users2,
+            href: "/dashboard/customers"
+        },
+        {
+            title: "Markets",
+            icon:Warehouse,
+            href: "/dashboard/markets"
+        },
+        {
+            title: "Farmers",
+            icon: UserSquare2,
+            href: "/dashboard/farmers"
+        },
+        {
+            title: "Orders",
+            icon: Compass,
+            href: "/dashboard/orders"
+        },
+        {
+            title: "Our Staff",
+            icon: User,
+            href: "/dashboard/staff"
+        },
+        {
+            title: "Settings",
+            icon: Settings,
+            href: "/dashboard/settings"
+        },
+        {
+            title: "Online Store",
+            icon: ExternalLink,
+            href: "/"
+        },
+    ]
+
+    const catalogueLinks = [
+        {
+            title: "Products",
+            icon:Users2,
+            href: "/dashboard/products"
+        },
+        {
+            title: "Categories",
+            icon:Warehouse,
+            href: "/dashboard/categories"
+        },
+        {
+            title: "Attributes",
+            icon: UserSquare2,
+            href: "/dashboard/attributes"
+        },
+        {
+            title: "Coupons",
+            icon: Slack,
+            href: "/dashboard/coupons"
+        },
+        {
+            title: "store sliders",
+            icon: User,
+            href: "/dashboard/sliders"
+        }
+    ]
+
   return (
-    <div className='dark:bg-slate-700 bg-white w-60 space-y-6 h-screen dark:text-slate-50 text-slate-800 p-3 fixed top-0 left-0 shadow-md'>
-      <Link className='mb-6' href="#">Logo</Link>
-      <div className="space-y-3 flex flex-col">
-        <Link href="#">Dashboard</Link>
-        <Link href="#">Catalogue</Link>
-        <Link href="#">Customers</Link>
-        <Link href="#">Markets</Link>
-        <Link href="#">Farmers</Link>
-        <Link href="#">Orders</Link>
-        <Link href="#">Staff</Link>
-        <Link href="#">Settings</Link>
-        <Link href="#">Online Store</Link>
-        <Link href="#">Staff</Link>
+    <div className= 'w-64 dark:bg-slate-700 bg-white space-y-6 h-screen dark:text-slate-50 text-slate-800 fixed top-0 left-0 shadow-md'>
+      <Link className='px-6 py-4' href="#">
+        <Image src={logo} alt='logo' className='w-36'/>
+      </Link>
+      <div className="space-y-3 flex flex-col mt-14 ">
+        <Link href="/dashboard" className={pathname==='/dashboard'? "flex items-center space-x-3 px-6 py-2 border-l-4 text-lime-500 border-lime-500": "flex items-center space-x-3 px-6 py-2"}>
+            <LayoutGrid/>
+            <span>Dashboard</span>
+        </Link>
+
+        <Collapsible>
+            <CollapsibleTrigger>
+                <button className="flex items-center space-x-6 px-6 py-2">
+                    <div className="flex items-center space-x-3">
+                        <Slack/>
+                        <span>Catalogue</span>
+                    </div>
+                    <ChevronRight/>
+                </button>
+            </CollapsibleTrigger>
+            <CollapsibleContent>
+                {
+                    catalogueLinks.map( (link, index) => (
+                        <Link key={index} href={link.href} className={pathname===link.href? "flex items-center space-x-3 px-6 py-2 border-l-4 text-lime-500 border-lime-500": "flex items-center space-x-3 px-6 py-2"}>
+                            <LayoutGrid/>
+                            <span>{link.title}</span>
+                        </Link>
+                    ))
+                }
+            </CollapsibleContent>
+        </Collapsible>
+
+        {
+            sidebarLinks.map((item, i) => {
+                const Icon = item.icon
+                return (
+                    <Link key={i} href={item.href} className={item.href===pathname? "flex items-center space-x-3 px-6 py-2 border-l-4 text-lime-500 border-lime-500": "flex items-center space-x-3 px-6 py-2"}>
+                        <Icon />
+                        <span>{item.title}</span>
+                    </Link>
+                )
+            })
+        }
+        <div className='py-2 px-6'>
+            <button className='bg-lime-600 rounded-md flex items-center space-x-3 py-3 px-6'>
+                <LogOut/>
+                Log Out
+            </button>
+        </div>
       </div>
     </div>
   )
